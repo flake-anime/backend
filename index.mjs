7c96ec2c88d3cc19ad693baa75e4d108089787f3
dusklight00 <r.rahul.developer@gmail.com>
2022-04-22 07:57:58 +0530

    search endpoint complete

---
+++ index.mjs
@@ -0,0 +1,21 @@
+import express from "express"
+import anime from "./models/anime.mjs"
+import mongoose from 'mongoose'
+import "dotenv/config"
+
+const MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING
+const PORT = process.env.PORT || 3000
+
+mongoose.connect(MONGO_CONNECTION_STRING)
+
+const app = express()
+
+app.get("/search", async (req, res) => {
+    const q = req.query.q
+    const result = await anime.find({$text: {$search: q}})
+    res.json(result)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
