8487ad9e32201e12ba0eae844c8a88cccb70e232
dusklight00 <r.rahul.developer@gmail.com>
2022-06-21 23:33:33 +0530

    similarity reordering implemented in search result

--- index.mjs
+++ index.mjs
@@ -1,72 +1,81 @@
-import express from "express"
-import anime from "./models/anime.mjs"
-import mongoose from 'mongoose'
-import "dotenv/config"
+import express from "express";
+import anime from "./models/anime.mjs";
+import mongoose from "mongoose";
+import similarity from "similarity";
+import "dotenv/config";
 
-const MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING
-const PORT = process.env.PORT || 4000
+const MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING;
+const PORT = process.env.PORT || 4000;
 
-mongoose.connect(MONGO_CONNECTION_STRING)
+mongoose.connect(MONGO_CONNECTION_STRING);
 
-const app = express()
+const app = express();
 
 app.get("/search", async (req, res) => {
-    const query = req.query.q
+  const query = req.query.q;
 
-    if (!query) {
-        return res.status(400).send("Query is required")
-    }
+  if (!query) {
+    return res.status(400).send("Query is required");
+  }
 
-    const result = await anime.find({$text: {$search: query}})
-    const filtered_result = result.map(item => {
-        return {
-            anime_name: item.anime_name,
-            gogo_id: item.gogo_id,
-            cover: item.cover,
-            type: item.type,
-            score: item.score,
-            total_episodes: item.episodes.length
-        }
-    })
-    res.json(filtered_result)
-})
+  const result = await anime.find({ $text: { $search: query } });
+  const filtered_result = result.map((item) => {
+    return {
+      anime_name: item.anime_name,
+      gogo_id: item.gogo_id,
+      cover: item.cover,
+      type: item.type,
+      score: item.score,
+      total_episodes: item.episodes.length,
+    };
+  });
+  const sorted_filter_result = filtered_result.sort((a, b) => {
+    const similartyA = similarity(a.anime_name, query);
+    const similartyB = similarity(b.anime_name, query);
+    return similartyB - similartyA;
+  });
+
+  res.json(sorted_filter_result);
+});
 
 app.get("/detail", async (req, res) => {
-    const gogo_id = req.query.gogo_id
-    const result = JSON.parse(JSON.stringify(await anime.findOne({gogo_id: gogo_id})))
-    if (result === null) {
-        return res.status(404).send("Anime not found")
-    }
-    const filtered_result = {
-        _id: result._id,
-        anime_name: result.anime_name,
-        gogo_id: result.gogo_id,
-        genres: result.genres,
-        cover: result.cover,
-        type: result.type,
-        plot_summary: result.plot_summary,
-        release: result.release,
-        status: result.status,
-        other_name: result.other_name,
-        trailer: result.trailer,
-        score: result.score,
-        url: result.url,
-    }
-    res.json(filtered_result)
-})
+  const gogo_id = req.query.gogo_id;
+  const result = JSON.parse(
+    JSON.stringify(await anime.findOne({ gogo_id: gogo_id }))
+  );
+  if (result === null) {
+    return res.status(404).send("Anime not found");
+  }
+  const filtered_result = {
+    _id: result._id,
+    anime_name: result.anime_name,
+    gogo_id: result.gogo_id,
+    genres: result.genres,
+    cover: result.cover,
+    type: result.type,
+    plot_summary: result.plot_summary,
+    release: result.release,
+    status: result.status,
+    other_name: result.other_name,
+    trailer: result.trailer,
+    score: result.score,
+    url: result.url,
+  };
+  res.json(filtered_result);
+});
 
 app.get("/get_episodes", async (req, res) => {
-    const gogo_id = req.query.gogo_id
-    const result = await anime.findOne({gogo_id: gogo_id})
+  const gogo_id = req.query.gogo_id;
+  const result = await anime.findOne({ gogo_id: gogo_id });
 
-    if (result === null) {
-        return res.status(404).send("Anime not found")
-    }
+  if (result === null) {
+    return res.status(404).send("Anime not found");
+  }
 
-    const filtered_result = result.episodes
-    res.json(filtered_result)
-})
+  const filtered_result = result.episodes;
+  res.json(filtered_result);
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
